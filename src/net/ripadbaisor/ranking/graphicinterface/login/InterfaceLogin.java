49613e8343570f50abcd0ab593b22cf104207b49
Juan Manuel <usr3507@salesianos-lacuesta.net>
2025-05-16 13:01:03 +0100

    Feat(InterfaceLogin.java): Added the login panel for the admin and client accounts

--- src/net/ripadbaisor/ranking/graphicinterface/login/InterfaceLogin.java
+++ src/net/ripadbaisor/ranking/graphicinterface/login/InterfaceLogin.java
@@ -43,7 +43,7 @@ public class InterfaceLogin extends JFrame {
     }
 
     private JPanel createInitialPanel() {
-        
+
         JPanel panel = new JPanel();
         panel.setBackground(Color.DARK_GRAY);
         panel.setLayout(new GridBagLayout());
@@ -84,7 +84,204 @@ public class InterfaceLogin extends JFrame {
 
     private JPanel createLoginPanel(String tipo) {
 
-        return new JPanel();
+        JPanel loginPanel = new JPanel();
+        loginPanel.setLayout(new GridBagLayout());
+
+        loginPanel.setBackground(Color.DARK_GRAY);
+
+        JLabel labelUser = new JLabel("Usuario:");
+        JTextField textUser = new JTextField();
+        JLabel labelPassword = new JLabel("Contraseña:");
+        JPasswordField textPassword = new JPasswordField();
+        JButton btnEnter = new JButton("Ingresar");
+        JButton btnReturn = new JButton("Regresar");
+        JLabel errorLabel = new JLabel("Usuario o contraseña incorrectos");
+
+        labelUser.setForeground(Color.WHITE);
+        labelPassword.setForeground(Color.WHITE);
+        textUser.setPreferredSize(new Dimension(180, 30));
+        textPassword.setPreferredSize(new Dimension(180, 30));
+        btnEnter.setPreferredSize(new Dimension(120, 50));
+        btnEnter.setBackground(new Color(111, 54, 154));
+        btnEnter.setForeground(Color.WHITE);
+        btnReturn.setPreferredSize(new Dimension(120, 50));
+        btnReturn.setBackground(new Color(111, 54, 154));
+        btnReturn.setForeground(Color.WHITE);
+        errorLabel.setForeground(Color.RED);
+        errorLabel.setVisible(false);
+
+        JLabel labelLogin;
+        if (tipo.toLowerCase().equals("admin")) {
+
+            labelLogin = new JLabel("Login de administrador");
+            labelLogin.setForeground(Color.WHITE);
+
+            GridBagConstraints gbc = new GridBagConstraints();
+            GridBagConstraints gbcTwo = new GridBagConstraints();
+            gbc.insets = new Insets(10, 10, 10, 10);
+            gbcTwo.insets = new Insets(10, 10, 30, -180);
+            gbcTwo.gridx = 0;
+            gbcTwo.gridy = 0;
+            loginPanel.add(labelLogin, gbcTwo);
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            loginPanel.add(labelUser, gbc);
+            gbc.gridx = 1;
+            gbc.gridy = 1;
+            loginPanel.add(textUser, gbc);
+            gbc.gridx = 0;
+            gbc.gridy = 2;
+            loginPanel.add(labelPassword, gbc);
+            gbc.gridx = 1;
+            gbc.gridy = 2;
+            loginPanel.add(textPassword, gbc);
+            gbc.gridx = 0;
+            gbc.gridy = 3;
+            loginPanel.add(btnEnter, gbc);
+            gbc.gridx = 1;
+            gbc.gridy = 3;
+            loginPanel.add(btnReturn, gbc);
+            gbcTwo.gridx = 0;
+            gbcTwo.gridy = 4;
+            loginPanel.add(errorLabel, gbcTwo);
+
+        } else {
+
+            labelLogin = new JLabel("Login de cliente");
+            JButton btnRegister = new JButton("Registarse");
+            btnRegister.setPreferredSize(new Dimension(120, 50));
+            btnRegister.setBackground(new Color(111, 54, 154));
+            btnRegister.setForeground(Color.WHITE);
+
+            labelLogin.setForeground(Color.WHITE);
+
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.insets = new Insets(10, 10, 10, 10);
+            gbc.gridx = 1;
+            gbc.gridy = 0;
+            loginPanel.add(labelLogin, gbc);
+            gbc.insets = new Insets(10, 10, 10, -100);
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            loginPanel.add(labelUser, gbc);
+            gbc.insets = new Insets(10, -100, 10, 10);
+            gbc.gridx = 2;
+            gbc.gridy = 1;
+            loginPanel.add(textUser, gbc);
+            gbc.insets = new Insets(10, 10, 10, -100);
+            gbc.gridx = 0;
+            gbc.gridy = 2;
+            loginPanel.add(labelPassword, gbc);
+            gbc.insets = new Insets(10, -100, 10, 10);
+            gbc.gridx = 2;
+            gbc.gridy = 2;
+            loginPanel.add(textPassword, gbc);
+            gbc.insets = new Insets(10, 10, 10, 10);
+            gbc.gridx = 0;
+            gbc.gridy = 3;
+            loginPanel.add(btnRegister, gbc);
+            gbc.gridx = 1;
+            gbc.gridy = 3;
+            loginPanel.add(btnEnter, gbc);
+            gbc.gridx = 2;
+            gbc.gridy = 3;
+            loginPanel.add(btnReturn, gbc);
+            gbc.insets = new Insets(10, -100, 10, -100);
+            gbc.gridx = 1;
+            gbc.gridy = 4;
+            loginPanel.add(errorLabel, gbc);
+
+            btnRegister.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+
+                    errorLabel.setVisible(false);
+
+                    setTitle("Registro");
+
+                    cardLayout.show(cardPanel, "ClientRegister");
+                }
+            });
+
+        }
+
+        btnReturn.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                errorLabel.setVisible(false);
+                setTitle("Interfaz de Login");
+                cardLayout.show(cardPanel, "Inicio");
+            }
+        });
+
+        btnEnter.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                String user = textUser.getText();
+                String password = new String(textPassword.getPassword());
+
+                if (user.isEmpty() || password.isEmpty()) {
+
+                    errorLabel.setVisible(true);
+
+                } else {
+
+                    if (tipo.toLowerCase().equals("admin")) {
+
+                        if (user.equals(credentialsList.get(0).getUsername())
+                                && password.equals(credentialsList.get(0).getPassword())) {
+
+                            textUser.setText("");
+                            textPassword.setText("");
+
+                            System.out.println("funciona");
+
+                        } else {
+
+                            errorLabel.setVisible(true);
+
+                        }
+
+                    } else {
+
+                        boolean gotLooped = false;
+
+                        for (int i = 1; i < credentialsList.size(); i++) {
+
+                            gotLooped = true;
+
+                            if (user.equals(credentialsList.get(i).getUsername())
+                                    && password.equals(credentialsList.get(i).getPassword())) {
+
+                                textUser.setText("");
+                                textPassword.setText("");
+
+                                System.out.println("funciona");
+
+                                errorLabel.setVisible(false);
+
+                            } else {
+
+                                errorLabel.setVisible(true);
+
+                            }
+                        }
+
+                        if (!gotLooped) {
+
+                            textUser.setText("");
+                            textPassword.setText("");
+
+                            errorLabel.setVisible(true);
+
+                        }
+
+                    }
+                }
+
+            }
+        });
+
+        return loginPanel;
     }
 
     private JPanel createRegisterPanel() {
